7ea7a7c78285504eacb7df271a12fd1b2a92c781
Asmaa Elkholy <asmaaelkholy6@gmail.com>
2025-01-22 12:20:37 +0300

    Initial commit

---
+++ Sources/externalIOSPackage/MyUtility.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  externalIOSPackage
+//
+//  Created by MacBook Pro on 22/07/1446 AH.
+//
+
+import Foundation
+public struct MyUtility {
+    public static func greet(name: String) -> String {
+        return "Hello, \(name)!"
+    }
+}
